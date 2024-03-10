854d3d53c75845cf5a51d50b5a6886a8c2d3bdf3
Emma A. A. Rünzel <emma@ruenzel.de>
2024-03-10 18:52:08 +0100

    Introduce GitHub Username Validation f/ User Model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  GITHUB_USERNAME_VALIDATION = /\A(?!\.)(?!_)[a-z0-9_\.]+(?<!\.)(?<!_)\z/
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -6,4 +8,6 @@ class User < ApplicationRecord
 
   has_many :friendships
   has_many :friends, through: :friendships
+
+  validates :username, uniqueness: true, format: { with: GITHUB_USERNAME_VALIDATION, message: "Only allows lowercase letters 🙇🏼‍♀️" }
 end

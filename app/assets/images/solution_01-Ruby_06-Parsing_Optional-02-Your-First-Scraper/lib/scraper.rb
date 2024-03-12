f01262d84f8a8a2ba9934488c572a618ae7741f6
Emma A. A. Rünzel <emma@ruenzel.de>
2024-03-13 00:00:43 +0100

    Delete Parsing Challenge Solution from App (LOL)

--- app/assets/images/solution_01-Ruby_06-Parsing_Optional-02-Your-First-Scraper/lib/scraper.rb
+++
@@ -1,8 +0,0 @@
-require "open-uri"
-require "nokogiri"
-
-def scrape_craiglist_antiques(city)
-  # TODO: return an array of Antiques found of Craigslist for this `city`.
-  doc = Nokogiri::HTML.parse(URI.open("http://#{city}.craigslist.org/search/ata"))
-  doc.search('.hdrlnk').map(&:text)
-end

d555f124b7ec3e43a53bdda7a441dedc3cbb87d5
Emma A. A. Rünzel <emma@ruenzel.de>
2024-03-16 22:31:08 +0100

    Update Contact Model w/ Validations for All Attributes & Relationship Inclusion

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,9 @@
 class Contact < ApplicationRecord
   belongs_to :user
+
+  validates :user_id, presence: true
+  validates :name, :relationship,  presence: true
+  validates :name, :relationship, :email, :phone_number, :address, :social_media_handle, presence: true
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP, message: 'Must be a valid email address 📪' }
+  validates :relationship, inclusion: { in: %w[friend family colleague], message: "%{value} is not a valid relationship" }
 end

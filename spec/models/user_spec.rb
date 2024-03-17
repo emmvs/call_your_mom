3d85e6d3bbe1fd1c7a1dc3681281ec55d6189d7d
Emma A. A. Rünzel <emma@ruenzel.de>
2024-03-17 12:47:44 +0100

    Update UserSpec Tests to Use User Factory for Consistent & Reliable Testing

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,41 +1,34 @@
-require "rails_helper.rb"
+require "rails_helper"
 
 RSpec.describe User, type: :model do
   it 'is valid with all required attributes' do
-    user = User.new(
-      first_name: 'Emma',
-      middle_name: 'Anna Agneta',
-      last_name: 'Rünzel',
-      username: 'emmvs',
-      email: 'emma@test.com',
-      password: '123456',
-    )
+    user = build(:user)
     expect(user).to be_valid
   end
 
   it 'is invalid without a first_name' do
-    user = User.new(first_name: nil)
+    user = build(:user, first_name: nil)
     expect(user).not_to be_valid
   end
 
   it 'is invalid without an email' do
-    user = User.new(email: nil)
+    user = build(:user, email: nil)
     expect(user).not_to be_valid
   end
 
   it 'is invalid without a password' do
-    user = User.new(password: nil)
+    user = build(:user, password: nil)
     expect(user).not_to be_valid
   end
 
   it 'is invalid with an incorrect email format' do
-    user = User.new(email: 'invalid_email')
+    user = build(:user, email: 'invalid_email')
     expect(user).not_to be_valid
   end
 
   it 'has a unique username' do
-    User.create(username: "emmvs", first_name: 'Emma', email: 'emma@test.com', password: '123456')
-    user = User.new(username: "emmvs", first_name: 'Santi', email: 'santi@test.com', password: '123456')
+    create(:user, username: "unique_username")
+    user = build(:user, username: "unique_username")
     expect(user).not_to be_valid
   end
 end

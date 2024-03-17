93442920cf707031872ef01e85c555f085244b48
Emma A. A. Rünzel <emma@ruenzel.de>
2024-03-17 12:45:46 +0100

    Move Enum Friendship Status Declaration to Top f/ Consistency in Convention

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,8 +1,8 @@
 class Friendship < ApplicationRecord
+  enum status: { requested: 0, accepted: 1, declined: 2 }
+
   belongs_to :user
   belongs_to :friend, class_name: 'User'
 
   validates :user, uniqueness: { scope: :friend, message: "Friendship already exists 👯" }
-
-  enum status: { requested: 0, accepted: 1, declined: 2 }
 end

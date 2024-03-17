31d3d2e7b2f53c9ae904ad9aa56a5bf8052406f9
Emma A. A. Rünzel <emma@ruenzel.de>
2024-03-17 12:46:23 +0100

    Add Validations & Associations to Medium Model w/ Static Media Types Array

--- app/models/medium.rb
+++ app/models/medium.rb
@@ -1,5 +1,8 @@
 class Medium < ApplicationRecord
-  MEDIA_TYPES = ['In Person', 'Phone Call', 'Text Message', 'Voice Message', 'Social Media', 'Gaming', 'Email', 'Meme']
+  MEDIA_TYPES = ['In Person', 'Phone Call', 'Text Message', 'Voice Message', 'Social Media', 'Gaming', 'Email', 'Meme'].freeze
 
   belongs_to :user
+  has_many :interactions, dependent: :destroy
+
+  validates :name, inclusion: { in: MEDIA_TYPES, message: "%{value.capitalize} is not a valid medium 🔮" }
 end

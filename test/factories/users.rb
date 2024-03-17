55a1ad76edc532670e31579bf5a32bab08a5208a
Emma A. A. Rünzel <emma@ruenzel.de>
2024-03-17 12:47:54 +0100

    Enhance User Factory w/ Name Attributes & Usernames for Richer Test Data

--- test/factories/users.rb
+++ test/factories/users.rb
@@ -1,7 +1,10 @@
 FactoryBot.define do
   factory :user do
-    sequence(:username) { |i| "username_#{i}"}
-    sequence(:email) { |i| "test_#{i}@gmail.com"}
+    sequence(:username) { |i| "username_#{i}" }
+    sequence(:email) { |i| "test_#{i}@gmail.com" }
     password { "123123" }
+    first_name { Faker::Name.first_name }
+    middle_name { Faker::Name.middle_name }
+    last_name { Faker::Name.last_name }
   end
 end

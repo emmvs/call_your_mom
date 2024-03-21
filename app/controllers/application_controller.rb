e071933988b9b50bc370c58547239884c9fb348a
Emma A. A. Rünzel <emma@ruenzel.de>
2024-03-21 17:36:36 +0100

    Implement User Time Zone Setting f/ Personalized User Experience

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_time_zone, if: :user_signed_in?
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
@@ -8,5 +9,15 @@ class ApplicationController < ActionController::Base
 
     # For additional in app/views/devise/registrations/edit.html.erb
     devise_parameter_sanitizer.permit(:account_update, keys: [:username, :email, :first_name, :middle_name, :last_name, :nickname, :emoji])
+  end
+
+  private
+  
+  # Sets the application's time zone based on the current user's settings. This method is a part of ensuring 
+  # that the user experience is consistent and personalized across the application. By using the time zone 
+  # set in the user's profile, all time-related data will reflect the user's local time.
+  # https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
+  def set_time_zone
+    Time.zone = current_user.user_setting.time_zone if current_user.user_setting&.time_zone.present?
   end
 end

b12cec2fecca94ed320413faf75ac7e4fb0b201c
Emma A. A. Rünzel <emma@ruenzel.de>
2024-03-21 18:34:15 +0100

    Add Relationship Options Method for Dynamic Relationship Selection in Contact Form

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -10,4 +10,8 @@ class Contact < ApplicationRecord
   validates :name, :relationship, :email, :phone_number, :address, :social_media_handle, presence: true
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP, message: 'Must be a valid email address 📪' }
   validates :relationship, inclusion: { in: %w[friend family colleague], message: "%{value} is not a valid relationship" }
+  
+  def self.relationship_options
+    [:friend, :family, :colleague]
+  end
 end

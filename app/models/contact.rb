05241848e9a47673fbe059de676dfd591abe1b12
Emma A. A. Rünzel <emma@ruenzel.de>
2024-03-26 17:20:00 +0100

    Update Contact Model Validations & Relationship Types

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,17 +1,12 @@
 class Contact < ApplicationRecord
-  RELATIONSHIP_TYPES = %w[friend family colleague]
+  RELATIONSHIP_TYPES = %i[friend family colleague enemy].freeze
 
   belongs_to :user
-
   has_many :interactions, dependent: :destroy
 
-  validates :user_id, presence: true
-  validates :name, :relationship,  presence: true
-  validates :name, :relationship, :email, :phone_number, :address, :social_media_handle, presence: true
+  validates :name, :relationship, :user_id, presence: true
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP, message: 'Must be a valid email address 📪' }
-  validates :relationship, inclusion: { in: %w[friend family colleague], message: "%{value} is not a valid relationship" }
-  
-  def self.relationship_options
-    [:friend, :family, :colleague]
-  end
+  validates :relationship, inclusion: {
+    in: RELATIONSHIP_TYPES.map(&:to_s), message: "%<value>s is not a valid relationship"
+  }
 end

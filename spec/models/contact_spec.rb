bfc32736b4ef7f04adfbf6c9124ae6593a351493
Emma A. A. Rünzel <emma@ruenzel.de>
2024-03-27 13:40:00 +0100

    Refactor Contact Model Specs f/ Compliance w/ Rubocop Standards

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -1,18 +1,19 @@
 RSpec.describe Contact, type: :model do
   subject(:contact) { build(:contact) }
 
-  describe 'associations and validations' do
-    it { is_expected.to belong_to(:user) }
+  describe 'associations' do
+    it { is_expected.to belong_to(:user).required(true) }
+  end
+
+  describe 'validations' do
     it { is_expected.to validate_presence_of(:name) }
     it { is_expected.to validate_presence_of(:relationship) }
     it { is_expected.to validate_presence_of(:user_id) }
     it { is_expected.to allow_value('example@example.com').for(:email) }
     it { is_expected.not_to allow_value('not-an-email').for(:email) }
-    it { is_expected.to allow_value('friend', 'family', 'colleague', 'enemy').for(:relationship) }
+    it { is_expected.to allow_values('friend', 'family', 'colleague', 'enemy').for(:relationship) }
     it { is_expected.not_to allow_value('idiot').for(:relationship) }
-  end
 
-  describe 'custom validations' do
     context 'with invalid email format' do
       before { contact.email = 'invalid_email_format' }
       it 'is invalid and has an appropriate error message' do

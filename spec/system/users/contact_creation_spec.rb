fdb9355f502ad90a182c055b1bad4708a33d3032
Emma A. A. Rünzel <emma@ruenzel.de>
2024-04-01 20:37:53 +0200

    Update Tests w/ Less Validations f/ Contacts

--- spec/system/users/contact_creation_spec.rb
+++ spec/system/users/contact_creation_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe "Contact creation", type: :system do
   ].each do |example|
     it "displays the form in #{example[:user]}'s preferred language" do
       user = send(example[:user])
-      login_as(user, scope: :user)
+      login_as(user)
       visit new_contact_path
 
       expect(page).to have_content(example[:content])
@@ -20,11 +20,11 @@ RSpec.describe "Contact creation", type: :system do
   end
 
   def user_setting_with_preferred_language(preferred_language)
-    FactoryBot.create(:user_setting, preferred_language:)
+    create(:user_setting, preferred_language:)
   end
 
-  let(:english_user) { FactoryBot.create(:user, user_setting: user_setting_with_preferred_language('en')) }
-  let(:german_user) { FactoryBot.create(:user, user_setting: user_setting_with_preferred_language('de')) }
-  let(:spanish_user) { FactoryBot.create(:user, user_setting: user_setting_with_preferred_language('es')) }
-  let(:swedish_user) { FactoryBot.create(:user, user_setting: user_setting_with_preferred_language('sv')) }
+  let(:english_user) { create(:user, user_setting: user_setting_with_preferred_language('en')) }
+  let(:german_user) { create(:user, user_setting: user_setting_with_preferred_language('de')) }
+  let(:spanish_user) { create(:user, user_setting: user_setting_with_preferred_language('es')) }
+  let(:swedish_user) { create(:user, user_setting: user_setting_with_preferred_language('sv')) }
 end

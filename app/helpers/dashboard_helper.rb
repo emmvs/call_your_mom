738d7aa94d9d9b3d0045682bb5cf841be2c7e5b8
Emma A. A. Rünzel <emma@ruenzel.de>
2024-04-01 22:20:27 +0200

    Implement Review Comments

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -1,34 +1,50 @@
 module DashboardHelper
   def greeting_for_current_user
-    if user_signed_in?
-      "#{time_based_greeting}#{current_user.nickname} #{time_based_emoji}"
-    else
-      "#{t('greetings.sunshine')}#{time_based_emoji}"
-    end
-  end
+    greeting = if user_signed_in?
+                 "#{t("greetings.#{day_attributes[:time_of_day]}")} #{current_user.nickname} #{day_attributes[:emoji]}"
+               else
+                 "#{t('greetings.sunshine')} #{emoji}"
+               end
 
-  def time_based_greeting
-    time_of_day = case Time.current.in_time_zone(user_time_zone).hour
-                  when 0...12 then 'morning'
-                  when 12...18 then 'day'
-                  when 18...22 then 'evening'
-                  else 'night'
-                  end
-    t("greetings.#{time_of_day}")
-  end
-
-  def time_based_emoji
-    case Time.current.in_time_zone(user_time_zone).hour
-    when 0...12 then '🌱'
-    when 12...18 then '☀️'
-    when 18...22 then '🌈'
-    else '🌙'
-    end
+    # Avoids potential cross-site scripting (XSS) attacks
+    greeting.html_safe
   end
 
   private
 
+  def day_attributes
+    if morning_range.cover?(current_hour)
+      {time_of_day: 'morning', emoji: '🌱'}
+    elsif afternoon_range.cover?(current_hour)
+      {time_of_day: 'day', emoji: '☀️'}
+    elsif evening_range.cover?(current_hour)
+      {time_of_day: 'evening', emoji: '🌈'}
+    else
+      {time_of_day: 'night', emoji: '🌙'} # TODO: Implement Moon depending on time of the month @josh
+    end
+  end
+
+  def current_hour
+    Time.current.in_time_zone(user_time_zone).hour
+  end
+
   def user_time_zone
     current_user.user_setting&.time_zone || 'Europe/Berlin'
+  end
+
+  def morning_range
+    0...12
+  end
+
+  def afternoon_range
+    12...18
+  end
+
+  def evening_range
+    18...22
+  end
+
+  def night_range
+    22...24
   end
 end

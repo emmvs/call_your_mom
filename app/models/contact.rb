799c96d58ecddaf2a3904ff870186c193b903637
Emma A. A. Rünzel <emma@ruenzel.de>
2024-04-02 13:37:33 +0200

    Update Contact Model w/ Better Validations & New DISPLAY_ATTRIBUTES Constant

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,10 +1,11 @@
 class Contact < ApplicationRecord
+  DISPLAY_ATTRIBUTES = %w[name email phone_number address relationship social_media_handle].freeze
   RELATIONSHIP_TYPES = %i[friend family colleague enemy].freeze
 
   belongs_to :user
   has_many :interactions, dependent: :destroy
 
-  validates :name, :relationship, :user_id, presence: true
+  validates :name, :relationship, presence: true
   validates :relationship, inclusion: {
     in: RELATIONSHIP_TYPES.map(&:to_s), message: "%<value>s is not a valid relationship"
   }

6476df41c0965764c0a71cf9f3c511f3c4c8b530
Joshua Smith <sarjsmithde@gmail.com>
2024-04-05 14:11:19 +0200

    Moved logic for building interaction and finding/creating medium to private methods in InteractionsController to improve code organization and readability.

--- app/controllers/interactions_controller.rb
+++ app/controllers/interactions_controller.rb
@@ -1,20 +1,31 @@
 class InteractionsController < ApplicationController
   def create
-    interaction = Interaction.new(contact_id: params[:interaction][:contact_id], interaction_date: params[:interaction][:interaction_date])
-    interaction.user = current_user
-
-    medium = Medium.find_or_create_by(name: params[:interaction][:medium], user: current_user)
-
+    interaction = build_interaction
+    medium = find_or_create_medium
     interaction.medium = medium
-
-    unless params[:interaction][:notes_attributes].values.first[:text].empty?
-      interaction.notes.build(params[:interaction][:notes_attributes].values.first)
-    end
+    build_notes(interaction)
 
     if interaction.save
-      redirect_to contact_path(interaction.contact_id)
+      redirect_to contact_path(interaction.contact_id), notice: "Interaction was successfully created."
     else
-      render :new
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def build_interaction
+    Interaction.new(contact_id: params[:interaction][:contact_id], interaction_date: params[:interaction][:interaction_date], user: current_user)
+  end
+
+  def find_or_create_medium
+    Medium.find_or_create_by(name: params[:interaction][:medium], user: current_user)
+  end
+
+  def build_notes(interaction)
+    params[:interaction][:notes_attributes]&.each_value do |note_params|
+      text = note_params[:text]
+      interaction.notes.build(text:) unless text.blank?
     end
   end
 end
